b9c217a0bf852badd26d26b5b452911cfc7cbcf1
Kaio-rgb <paiterschobiner@gmail.com>
2025-04-22 21:33:13 -0300

    adicao do java puxando as informacoes cadastro gasto e total

---
+++ main.js
@@ -0,0 +1,8 @@
+const formCadastro = document.getElementById('form-cadastro');
+const listaDeGastos = document.getElementById('lista-de-gastos');
+const totalElement = document.getElementById('total');
+let gastos = [];
+let idEdicao = null; 
+
+// Variável para rastrear o ID do gasto em edição
+
